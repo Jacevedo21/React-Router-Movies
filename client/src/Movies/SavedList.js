315b465a67d9fe8ba44b333176feb77c4b46ac2f
Jacevedo21 <jacevedo0213@gmail.com>
2020-07-20 19:07:47 -0400

    simple mistake

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import {useHistory, Link} from 'react-router-dom'
+import { useHistory, Link } from 'react-router-dom'
 
 
 
+const SavedList = props => (
 
-export default function SavedList(props) {
-
-    const history = useHistory();
-
-  const routeToHome = () => {
-  history.push('/Movies')
-  }
-  
-  return(
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
       <span className="saved-movie">{movie.title}</span>
     ))}
-    <div onclick={routeToHome} className="home-button">Home</div>
-  </div>
-)};
+    <Link to='/'>
 
-// export default SavedList;
+      <div className="home-button">Home</div>
+    </Link>
+  </div>
+);
+
+export default SavedList;
