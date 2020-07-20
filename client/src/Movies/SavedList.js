1d9a5fccb118e6a958388e696be39e30a4375975
Jacevedo21 <jacevedo0213@gmail.com>
2020-07-20 19:00:36 -0400

    minor bugs

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -2,21 +2,24 @@ import React from 'react';
 import {useHistory, Link} from 'react-router-dom'
 
 
-// const history = useHistory();
 
-// const routeToHome = () => {
-// history.push('/')
-// }
 
-const SavedList = props => (
+export default function SavedList(props) {
+
+    const history = useHistory();
+
+  const routeToHome = () => {
+  history.push('/Movies')
+  }
   
+  return(
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
       <span className="saved-movie">{movie.title}</span>
     ))}
-    <div className="home-button">Home</div>
+    <div onclick={routeToHome} className="home-button">Home</div>
   </div>
-);
+)};
 
-export default SavedList;
+// export default SavedList;
