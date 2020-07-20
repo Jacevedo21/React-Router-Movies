bed4676f8e3ebf98f11186924cd6a3c1219adfd9
Jacevedo21 <jacevedo0213@gmail.com>
2020-07-20 18:51:35 -0400

    Almost there

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+
 
 const MovieList = props => {
+  // const { movie } = props;
+  // const {url} = useRouteMatch()
+
   return (
-    <div className="movie-list">
-      {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
-      ))}
+      <div className="movie-list">
+        {props.movies.map(movie => (
+          <Link to={`/Movies/${movie.id}`}>
+            <MovieDetails key={movie.id} movie={movie} />
+          </Link>
+        ))}
+
     </div>
   );
 }
 
 function MovieDetails({ movie }) {
   const { title, director, metascore } = movie;
+  
   return (
     <div className="movie-card">
+        {/* <Link to={`${url}/${movie.id}`} /> */}
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import {useHistory, Link} from 'react-router-dom'
+
+
+// const history = useHistory();
+
+// const routeToHome = () => {
+// history.push('/')
+// }
 
 const SavedList = props => (
+  
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
